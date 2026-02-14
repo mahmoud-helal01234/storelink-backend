cf751ef7003cfa497d5e8afd52c8b862f748f9ae
mahmoud-helal01234 <mahmoud,helal01234@gmail.com>
2026-02-15 00:53:40 +0200

    store ad softdelete

--- app/Models/StoreAd.php
+++ app/Models/StoreAd.php
@@ -3,24 +3,25 @@
 namespace App\Models;
 
 use App\Http\Traits\FileUploadTrait;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class StoreAd extends Model
 {
     use HasFactory;
     use FileUploadTrait;
+    use SoftDeletes;
 
     protected $table = "stores_ads";
 
     protected $fillable = [
-        
+
         'id', 'image', 'description_ar','description_en','store_id', 'created_at', 'updated_at','active'];
 
     public function setImageAttribute($value){
         $this->attributes['image'] = $this->uploadFile($value, 'images/store_ads', $this->attributes['image'] ?? "");
     }
-
 
     public function store()
     {
