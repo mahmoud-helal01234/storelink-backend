de1bb0a7d1256cc0ace14fbeb83d97730674d513
mahmoud-helal01234 <mahmoud,helal01234@gmail.com>
2026-06-27 22:20:38 +0300

    optimize images quality before uploading

--- app/Http/Traits/FileUploadTrait.php
+++ app/Http/Traits/FileUploadTrait.php
@@ -6,29 +6,52 @@ use Illuminate\Support\Facades\File;
 
 trait FileUploadTrait
 {
-    protected function uploadFile ($file, $location ,$imagePath = null) {
+    protected function uploadFile($file, $location, $imagePath = null)
+    {
 
-        if($imagePath)
-        {
+        if ($imagePath) {
             $img = public_path($imagePath);
             File::delete($img);
         }
 
         $file_original_name = $file->getClientOriginalName();
         $file_original_extension = $file->getClientOriginalExtension();
-        $file_unique_name = time().rand(100,999) .'.'. $file_original_extension;
+        $file_unique_name = time() . rand(100, 999) . '.' . $file_original_extension;
         $new_path = $location;
         $folder_path = public_path($new_path);
-        $file -> move($folder_path, $file_unique_name);
-        return $new_path.'/'.$file_unique_name;
+
+        // Compress images, otherwise upload normally
+        if (in_array($file_original_extension, ['jpg', 'jpeg', 'png', 'webp'])) {
+
+            $manager = new \Intervention\Image\ImageManager(
+                new \Intervention\Image\Drivers\Gd\Driver()
+            );
+
+            $image = $manager->read($file);
+
+            $image->scaleDown(
+                width: 1920,
+                height: 1920
+            );
+
+            $path = $folder_path . DIRECTORY_SEPARATOR . $file_unique_name;
+
+            match ($file_original_extension) {
+                'jpg', 'jpeg' => $image->toJpeg(80)->save($path),
+                'png'         => $image->toPng()->save($path),
+                'webp'        => $image->toWebp(80)->save($path),
+            };
+        } else {
+            $file->move($folder_path, $file_unique_name);
+        }
+        return $new_path . '/' . $file_unique_name;
     }
 
-    public function deleteFile($filePath){
-        if($filePath)
-        {
+    public function deleteFile($filePath)
+    {
+        if ($filePath) {
             $img = public_path($filePath);
             File::delete($img);
         }
     }
-
 }
